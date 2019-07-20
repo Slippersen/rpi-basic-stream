f10bd1e09c24ea1bc1f74282c8c9555fba1567f6
slippersen <erik.sandal@live.no>
2019-07-20 12:18:45 +0200

    init

---
+++ pi_stream.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import picamera
+import time
+import io
+import os
+
+from flask import Flask, Response
+from flask_basicauth import BasicAuth
+
+
+app = Flask(__name__)
+
+app.config['BASIC_AUTH_USERNAME'] = os.environ['STREAMUSER']
+app.config['BASIC_AUTH_PASSWORD'] = os.environ['STREAMPWD']
+
+basic_auth = BasicAuth(app)
+
+
+@app.route('/')
+@basic_auth.required
+def index():
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def gen():
+    # video stream generator function
+    while True:
+        try:
+            with picamera.PiCamera() as camera:
+                time.sleep(2)   # wait for focus
+                stream = io.BytesIO()
+
+                for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+                    stream.seek(0)
+
+                    yield(b'--frame\r\n'
+                          b'Content-Type: image/jpeg\r\n\r\n' + stream.read() + b'\r\n')
+
+                    stream.seek(0)
+                    stream.truncate
+
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
