1397a67e74091bfef102a0dc2fda636494065f7c
slippersen <erik.sandal@live.no>
2019-07-21 11:42:01 +0200

    using template

--- pi_stream.py
+++ pi_stream.py
@@ -5,7 +5,7 @@ import time
 import io
 import os
 
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 from flask_basicauth import BasicAuth
 
 
@@ -20,6 +20,12 @@ basic_auth = BasicAuth(app)
 @app.route('/')
 @basic_auth.required
 def index():
+    return render_template('index.html')
+
+
+@app.route('/feed')
+@basic_auth.required
+def feed():
     return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
@@ -27,22 +33,21 @@ def gen():
     # video stream generator function
     while True:
         try:
-            with picamera.PiCamera() as camera:
-                time.sleep(2)   # wait for focus
-                stream = io.BytesIO()
+            time.sleep(2)   # wait for focus
+            stream = io.BytesIO()
 
-                for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
-                    stream.seek(0)
+            for _ in picamera.PiCamera().capture_continuous(stream, 'jpeg', use_video_port=True):
+                stream.seek(0)
 
-                    yield(b'--frame\r\n'
-                          b'Content-Type: image/jpeg\r\n\r\n' + stream.read() + b'\r\n')
+                yield(b'--frame\r\n'
+                        b'Content-Type: image/jpeg\r\n\r\n' + stream.read() + b'\r\n')
 
-                    stream.seek(0)
-                    stream.truncate
+                stream.seek(0)
+                stream.truncate
 
         except Exception as e:
             print(e)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+    app.run(host='0.0.0.0', threaded=True, debug=True)
